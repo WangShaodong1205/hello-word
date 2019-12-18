503b0f2ae5fb323c3193e9e4f612e9eef7d67838
WangShaodong1205 <hnzzwsd@163.com>
2019-12-18 20:16:05 +0800

    这是一个测试文件

---
+++ src/main/java/com/aaa/wang/springcloud/test.java
@@ -0,0 +1,12 @@
+package com.aaa.wang.springcloud;
+
+/**
+ * @Author WangShaodong
+ * @Date Create in 2019/12/18 20:13
+ * @Description
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
